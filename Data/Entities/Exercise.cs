ac9ab9d7df45ca86bd3aa213b7b7dff6291e4d3c
David Godi <David.Godi@greenstonefcs.com>
2020-01-30 19:13:24 -0500

    add ef datavase first entities

---
+++ Data/Entities/Exercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Data.Entities
+{
+    public class Exercise
+    {
+        public int ExerciseId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<LogExercise> LogExercises { get; set; }
+    }
+}
