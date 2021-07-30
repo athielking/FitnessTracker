913f869abdd729a5849b80142edf187df67e6beb
david_godi <david_godi@yahoo.com>
2021-07-29 23:17:01 -0400

    fix log filter to support observables.  wanted to try and use observable filter

--- fitness-tracker/src/app/logs/log-filter.pipe.ts
+++ fitness-tracker/src/app/logs/log-filter.pipe.ts
@@ -1,20 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Log } from '../shared/models/log';
+import { from, of, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ILog, Log } from '../shared/models/log';
+import { LogsModule } from './logs.module';
 
 @Pipe({ name: 'logFilter' })
 export class LogFilterPipe implements PipeTransform {
 
-  transform(items: Log[], searchText: string): any[] {
+  transform(items: Observable<ILog[]>, searchText: string): Observable<ILog[]> {
     if (!items) {
-      return [];
+      return of([]);
     }
     if (!searchText) {
       return items;
     }
     searchText = searchText.toLocaleLowerCase();
 
-    return items.filter(it => {
-      return it.user.userName.toLocaleLowerCase().includes(searchText);
-    });
+    return items.pipe(map(logs => logs.filter(log => log.user.userName.toLocaleLowerCase().includes(searchText))));
   }
 }
